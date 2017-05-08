f44925e477cddbe6399e7ce0c863a2d03b9b4695
Miriam Meier <meier.miriam@hotmail.com>
2017-05-08 10:56:55 +0200

    no message

--- index.php
+++ index.php
@@ -111,7 +111,7 @@
         <!-- Page Header -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Photoloca
+                <h1 class="page-header">Photoloca quando?
                     <small>Machen Sie sich ein Bild davon!</small>
                 </h1>
             </div>
