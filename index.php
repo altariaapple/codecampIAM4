1df0f5699756181db70c6d99b00f968a4240f0b1
Tim Glatthard <tim@tims-mbp.bfh.ch>
2017-05-08 12:02:14 +0200

    no message

--- index.php
+++ index.php
@@ -147,7 +147,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Photoloca 2016</p>
+                    <p>Copyright &copy; Photoloca 2017</p>
                 </div>
             </div>
             <!-- /.row -->
